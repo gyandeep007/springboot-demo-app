16f5246b0b5467a0c54bf0cfa7cf9d8c4cef7da6
gyandeep <gyandeepsharma007@gmail.com>
2023-06-24 12:50:40 +0530

    removed test classes

--- src/test/java/com/gyan/springbootdemoapp/SpringbootDemoAppApplicationTests.java
+++ src/test/java/com/gyan/springbootdemoapp/SpringbootDemoAppApplicationTests.java
@@ -3,7 +3,7 @@ package com.gyan.springbootdemoapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SpringbootDemoAppApplicationTests {
 
     @Test
